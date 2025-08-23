900f9f054f9d5f7f4de28c1a16a1f52f9e9e8ada
Joseph <202180024@psu.palawan.edu.ph>
2025-08-23 15:20:24 +0800

    eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,16 +11,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
-];
+  ...compat.config({
+    extends: ['next'],
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      '@next/next/no-page-custom-font': 'off',
+    },
+  }),
+]
 
 export default eslintConfig;
